6eb0bcd86ef61fc25940086a7fc00dcdfc4de49a
Ariel <aab-97@hotmail.com>
2019-12-02 04:12:36 -0700

    ADDED: Common user interface

--- server/views/js/myScript.js
+++ server/views/js/myScript.js
@@ -1,22 +1,23 @@
 var socket = io.connect('http://localhost:8080', { 'forceNew': true });
 
-socket.on('messages', (data)=> {
-  console.log(data);
-  data.forEach((item, index) => {
-    updatePlace(item);
-  });
+socket.on('messages', (data) => {
+    console.log(data);
+    data.forEach((item, index) => {
+        updatePlace(item);
+    });
 });
-socket.on('message',(data)=>{
-console.log(data);
-updatePlace(data);
+socket.on('message', (data) => {
+    console.log(data);
+    updatePlace(data);
 });
-function updatePlace(jsonObject){
-  var color = "red";
-  var text = "OCUPADO";
-  if(!jsonObject.status){
-    color = "green";
-    text = "LIBRE";
-  }
-  document.getElementById('Place'+jsonObject.idSensor).innerHTML = text;
-  document.getElementById('Place'+jsonObject.idSensor).style.color = color;
+
+function updatePlace(jsonObject) {
+    var color = "red";
+    var text = "OCUPADO";
+    if (!jsonObject.status) {
+        color = "green";
+        text = "LIBRE";
+    }
+    document.getElementById('Place' + jsonObject.idSensor).innerHTML = text;
+    document.getElementById(' ' + jsonObject.idSensor).style.color = color;
 }
