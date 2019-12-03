a1f68ae787af2ebf3e39dae551c72a84b4ae8f18
Eduardo <e>
2019-12-03 09:12:09 -0700

    Final v 1.0

--- mqtt/publisher.js
+++ mqtt/publisher.js
@@ -4,9 +4,9 @@ var status = true;
 client.on("connect", function() {
   setInterval(function() {
     var reading = {
-      sensorId: 3,
+      sensorId: 2,
       status: status
-    };
+    };  
     client.publish("readings", JSON.stringify(reading));
     console.log("Enviado: " + status);
     console.log(JSON.stringify(reading));
